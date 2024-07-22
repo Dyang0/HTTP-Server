5bc8eb9c507f9eb651f4b4152d2186a34eee614b
Daniel Yang (KaliVM) <you@example.com>
2024-07-21 21:37:23 -0400

    fixing Werrors

--- server_c/server.c
+++ server_c/server.c
@@ -19,6 +19,9 @@
 #define BUFFER_SIZE 104857600
 // test comment
 // git test
+
+const char *get_file_extension(const char *filename);
+
 const char *get_file_extension(const char *file_name) {
     const char *dot = strrchr(file_name, '.');
     if (!dot || dot == file_name) {
